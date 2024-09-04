1980852e04a7790ce412f39ce9a6b11afa270a40
chakupewa <chakupewajanuary38@gamil.com>
2024-09-04 10:16:19 +0300

    emma aploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Emma';
+
+  science=signal<any>('');
+
+
+
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
 }
