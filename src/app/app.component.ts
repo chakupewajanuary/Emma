d8c8a27cf149aaf43b43775046889bb2df4f3177
chakupewa <chakupewajanuary38@gamil.com>
2024-09-17 09:09:00 +0300

    tuesday 17th sept commited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,154 +1,124 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { KaribuComponent } from './components/karibu/karibu.component';
-// import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, FormsModule, CommonModule,HttpClientModule,KaribuComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'], // Fixed typo from "styleUrl" to "styleUrls"
 })
 export class AppComponent implements OnInit {
-[x: string]: any;
-  // title = 'Emma';
-  designationList:any [] = [];
-  roleList:any [] = [];
-
-    stepsList: any[] = [
+  designationList: any[] = [];
+  roleList: any[] = [];
+  stepsList: any[] = [
     { stepName: 'Basic Details', isCompleted: false },
     { stepName: 'Skills', isCompleted: false },
     { stepName: 'Experience', isCompleted: false },
   ];
 
   activeStep: any = this.stepsList[0];
-    // value to store percentage for complition
-  stepperCompletionValue:number=8;
-  //object
+  stepperCompletionValue: number = 8; // percentage for stepper progress
+
   employeeObj: any = {
-  "roleId": 0,
-  "userName": "string",
-  "empCode": "string",
-  "empId": 0,
-  "empName": "string",
-  "empEmailId": "string",
-  "empDesignationId": 0,
-  "empContactNo": "string",
-  "empAltContactNo": "string",
-  "empPersonalEmailId": "string",
-  "empExpTotalYear": 0,
-  "empExpTotalMonth": 0,
-  "empCity": "string",
-  "empState": "string",
-  "empPinCode": "string",
-  "empAddress": "string",
-  "empPerCity": "string",
-  "empPerState": "string",
-  "empPerPinCode": "string",
-  "empPerAddress": "string",
-  "password": "string",
+    "roleId": 0,
+    "userName": "",
+    "empCode": "",
+    "empId": 0,
+    "empName": "",
+    "empEmailId": "",
+    "empDesignationId": 0,
+    "empContactNo": "",
+    "empAltContactNo": "",
+    "empPersonalEmailId": "",
+    "empExpTotalYear": 0,
+    "empExpTotalMonth": 0,
+    "empCity": "",
+    "empState": "",
+    "empPinCode": "",
+    "empAddress": "",
+    "empPerCity": "",
+    "empPerState": "",
+    "empPerPinCode": "",
+    "empPerAddress": "",
+    "password": "",
     ErpEmployeeSkills: [],
     ErmEmpExperiences: [],
   };
-  // ErpEmployeeSkills: any = {
-  //   "empSkillId": 0,
-  //   "empId": 0,
-  //   "skill": "string",
-  //   "totalYearExp": 0,
-  //   "lastVersionUsed": "string"
-  // };
-  // ErmEmpExperiences: any = {
-  //     "empExpId": 0,
-  //     "empId": 0,
-  //     "companyName": "string",
-  //     "startDate": "2024-09-09T06:19:28.800Z",
-  //     "endDate": "2024-09-09T06:19:28.800Z",
-  //     "designation": "string",
-  //     "projectsWorkedOn": "string"
-  // };
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadDesignations();
+    this.loadRoleId();
+    console.log('this is my reoleId', this.employeeObj.reoleId)
+  }
 
   setActiveStep(activeStep: any) {
     this.activeStep = activeStep;
   }
-  addSkills(){
-    const skillObj={
+
+  addSkills() {
+    const skillObj = {
       "empSkillId": 0,
       "empId": 0,
-      "skill": "string",
+      "skill": "",
       "totalYearExp": 0,
-      "lastVersionUsed": "string"
+      "lastVersionUsed": ""
     };
-    console.log(skillObj);
     this.employeeObj.ErpEmployeeSkills.unshift(skillObj);
-    console.log(this.employeeObj.ErpEmployeeSkills)
   }
-  addExp(){
-    const expObj={
-        "empExpId": 0,
-        "empId": 0,
-        "companyName": "string",
-        "startDate": "2024-09-09T06:19:28.800Z",
-        "endDate": "2024-09-09T06:19:28.800Z",
-        "designation": "string",
-        "projectsWorkedOn": "string"
+
+  addExp() {
+    const expObj = {
+     
+      "empExpId": 0,
+      "empId": 0,
+      "companyName": "",
+      "startDate": "2024-09-13T06:18:51.691Z",
+      "endDate": "2024-09-13T06:18:51.691Z",
+      "designation": "",
+      "projectsWorkedOn": ""
     };
-    console.log(expObj);
     this.employeeObj.ErmEmpExperiences.unshift(expObj);
-    console.log( this.employeeObj.ErmEmpExperiences);
-    // this.cdr.detectChanges();    
   }
-  loadDesignations(){
-    this.http.get("https://freeapi.gerasim.in/api/EmployeeApp/GetAllDesignation").subscribe((res:any)=>{
-      this.designationList=res.data;
-      // console.log(this.designationList);
-    })
-  }
-  loadRoleId(){
-    this.http.get("https://freeapi.gerasim.in/api/EmployeeApp/GetAllRoles").subscribe((res:any)=>{
-      this.roleList=res.data;
-      // console.log(this.roleList);
-    })
-  }
-  gotoStep2(){
-    const currentStep=this.stepsList.find(m=>m.stepName==this.activeStep.stepName);
-    currentStep.isCompleted=true;
-    this.activeStep=this.stepsList[1];
-    this.stepperCompletionValue=50;
-  }
-  gotoStep3(){
-    const currentStep=this.stepsList.find(m=>m.stepName==this.activeStep.stepName);
-    currentStep.isCompleted=true;
-    this.activeStep=this.stepsList[2];
-    this.stepperCompletionValue=100;
-   }
 
-  //  saveEmployee() {
-  //   debugger;
-  //   console.log(this.employeeObj);
-  
-  //   this.http.post("https://freeapi.gerasim.in/api/EmployeeApp/CreateNewEmployee", this.employeeObj)
-  //     .subscribe(
-  //       (res: any) => {
-  //         if (res.result) {
-  //           alert('Employee Create Success');
-  //         } else {
-  //           alert(res.message || 'An error occurred while creating the employee');
-  //         }
-  //       },
-  //       (error) => {
-  //         // Error handler to catch network issues or invalid requests
-  //         console.error('Error posting employee:', error);
-  //         alert('Failed to create employee. Please try again.');
-  //       }
-  //     );
-  // }
-  
+  loadDesignations() {
+    this.http.get("https://freeapi.gerasim.in/api/EmployeeApp/GetAllDesignation")
+      .subscribe((res: any) => {
+        this.designationList = res.data;
+      });
+  }
 
- saveEmployee(){
+  loadRoleId() {
+    this.http.get("https://freeapi.gerasim.in/api/EmployeeApp/GetAllRoles")
+      .subscribe((res: any) => {
+        this.roleList = res.data;
+      });
+  }
+
+  gotoStep2() {
+    const currentStep = this.stepsList.find(m => m.stepName == this.activeStep.stepName);
+    if (currentStep) {
+      currentStep.isCompleted = true;
+      this.activeStep = this.stepsList[1];
+      this.stepperCompletionValue = 50;
+    }
+  }
+
+  gotoStep3() {
+    const currentStep = this.stepsList.find(m => m.stepName == this.activeStep.stepName);
+    if (currentStep) {
+      currentStep.isCompleted = true;
+      this.activeStep = this.stepsList[2];
+      this.stepperCompletionValue = 100;
+    }
+  }
+  saveEmployee(){
     debugger;
     
     this.http.post("https://freeapi.gerasim.in/api/EmployeeApp/CreateNewEmployee",this.employeeObj).subscribe((res:any)=>{
@@ -160,13 +130,7 @@ export class AppComponent implements OnInit {
       }
       console.log('this is my employee OBJECT', this.employeeObj)
     })
-  }
+  
 
-  constructor(private http:HttpClient){}
-  ngOnInit(): void {
-    this.loadDesignations() ;
-    this.loadRoleId()
   }
- 
-}
-
+ }
